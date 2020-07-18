b8622696f29d003a963996b616e2bc10fc65be07
cabana <cabanaxsiempre@gmail.com>
2020-07-18 01:29:45 -0400

    creacion del primer servicio get /user

--- routes/apirouter.js
+++ routes/apirouter.js
@@ -8,4 +8,29 @@ var users = new USER(params);
 var result = await users.save();
 res.status(200).json(result);
 });
+//creacion servicio get
+router.get("/user", (req, res) => {
+    var params = req.query;
+    console.log(params);
+    var limit = 100;
+    if (params.limit != null) {
+        limit = parseInt(params.limit);
+    }
+    var order = -1;
+    if (params.order != null) {
+     if (params.order == "desc") {
+        order = -1;
+    } else if (params.order == "asc") {
+        order = 1;
+      }
+    }
+    var skip = 10;
+    if (params.skip != null) {
+        skip = parseInt(params.skip);
+    }
+    USER.find({}).limit(limit).sort({_id: order}).skip(skip).exec((err, docs) => {
+     res.status(200).json(docs);
+    });
+});
+    
 module.exports = router;
