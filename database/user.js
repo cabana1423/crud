aff22dcf29c9174989d1797ec97910099b2ec1bb
cabana <cabanaxsiempre@gmail.com>
2020-07-18 00:37:14 -0400

    creacion del primer servicio post /user

--- database/user.js
+++ database/user.js
@@ -1,11 +1,11 @@
 const mongoose = require("./connect");
-var USERSCHEMA = {
+var USERSCHEMA =new mongoose.Schema({
 name : String,
 email : String,
 password : String,
 registerdate: Date,
 sex : String,
 address : String
-}
+});
 const USERS = mongoose.model("users", USERSCHEMA);
 module.exports = USERS;
---
+++ routes/apirouter.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var USER=require("../database/user");
+router.post('/user', async(req, res) => {
+var params = req.body;
+params["registerdate"] = new Date();
+var users = new USER(params);
+var result = await users.save();
+res.status(200).json(result);
+});
+module.exports = router;
